431ee32eef9805d1ef5adfdc81488fcdc5569ea3
vincentg <vincentgagha@gmail.com>
2021-12-01 19:25:56 +0800

    final-app

---
+++ src/Apps.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Routes, Route } from "react-router-dom";
+import Home from './pages/Home';
+import About from './pages/About';
+
+import './assets/item.css';
+
+const Apps = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+    </Routes>
+  );
+}
+
+export default Apps;
+//Untuk output
---
+++ src/pages/About.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Navigation from '../components/Navigation';
+import Footer from '../components/Footer';
+
+const About = () => {
+  return(
+    <>
+      <Navigation />
+
+      <div className="container my-5">
+        <h1>Aplikasi Untuk Mencari Anime</h1>
+        <p>Gaghana, Jopie Vincent Corinthians (105021910046), Informatika</p>
+      </div>
+
+      <Footer />
+    </>
+  );
+}
+
+export default About;
